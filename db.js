5ba5d1e36c1eeb06f6e564a3c053f282ef88b7c0
JASON ROUNDTREE <jasontree123@gmail.com>
2021-03-04 19:21:17 -0800

    Add PG prod config for DATABASE_URL

--- db.js
+++ db.js
@@ -1,16 +1,25 @@
 const { Pool } = require('pg')
 require('dotenv').config()
 
-const pool = new Pool({
-    connectionString: process.env.DB_CONNECTION_STRING,
-    // user: process.env.DB_USER,
-    // host: process.env.DB_HOST,
-    // database: process.env.DB_DATABASE,
-    // password: process.env.DB_PASSWORD,
-    // port: 5432,
+const devConfig = {
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: 5432,
     ssl: {
         rejectUnauthorized: false
     }
-})
+}
+
+// const devConfig = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
+
+const prodConfig = { 
+    connectionString: process.env.DATABASE_URL 
+}
+
+const pool = new Pool(
+    process.env.NODE_ENV === 'production' ? prodConfig : devConfig
+)
 
 module.exports = pool
